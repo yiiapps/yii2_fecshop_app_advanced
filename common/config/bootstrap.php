2fab21b12b86bce72b2bf0491d080e601bf14509
tbq <tbq2616@sina.com>
2019-10-17 15:17:11 +0800

    my zhanshen

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -14,23 +14,23 @@ Yii::setAlias('@google', dirname(dirname(__DIR__)) . '/vendor/fancyecommerce/fec
 Yii::setAlias('@addons', dirname(dirname(__DIR__)) . '/addons');
 
 // 下面的代码部分为：命令行执行sql初始化，不加载应用插件部分。
-$is_install = true;
-if (is_array($argv)) {
-    foreach ($argv as $av) {
-        if ($av == '--migrationPath=@fecshop/migrations/mysqldb') {
-            $is_install = true;
-            break;
-        }
-    }
-}
+// $is_install = false;
+// if (is_array($argv)) {
+//     foreach ($argv as $av) {
+//         if ($av == '--migrationPath=@fecshop/migrations/mysqldb') {
+//             $is_install = true;
+//             break;
+//         }
+//     }
+// }
 
-if (!$is_install) {
-    $dbConfig = isset($fecmall_common_main_local_config['components']['db']) ? $fecmall_common_main_local_config['components']['db'] : '';
-    if (is_array($dbConfig) && !empty($dbConfig)) {
-        $connection = Yii::createObject($dbConfig);
-        $command = $connection->createCommand('SELECT * FROM  extensions where  status=:status AND installed_status=:installed_status ORDER BY priority ASC ');
-        $command->bindValue(':status', 1);
-        $command->bindValue(':installed_status', 1);
-        $fecmall_db_extensions_data = $command->queryAll();
-    }
-}
+// if (!$is_install) {
+//     $dbConfig = isset($fecmall_common_main_local_config['components']['db']) ? $fecmall_common_main_local_config['components']['db'] : '';
+//     if (is_array($dbConfig) && !empty($dbConfig)) {
+//         $connection = Yii::createObject($dbConfig);
+//         $command = $connection->createCommand('SELECT * FROM  extensions where  status=:status AND installed_status=:installed_status ORDER BY priority ASC ');
+//         $command->bindValue(':status', 1);
+//         $command->bindValue(':installed_status', 1);
+//         $fecmall_db_extensions_data = $command->queryAll();
+//     }
+// }
